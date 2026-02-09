142027d598ca7b8a150a53bd20f31b0c40d4fcb7
space-lumps <165586177+space-lumps@users.noreply.github.com>
2026-02-08 22:32:50 -0500

    Add encounter count test

---
+++ sql/pipeline/tests/22_test_encounter_counts.sql
@@ -0,0 +1,19 @@
+
+WITH encounters_per_patient AS (
+	SELECT
+		patient_id
+		,COUNT(DISTINCT encounter_id) AS encounter_count
+	FROM cohort_output
+	GROUP BY
+		patient_id
+)
+
+SELECT
+	encounter_count
+	,COUNT(*) AS patient_count
+FROM encounters_per_patient
+GROUP BY
+	encounter_count
+ORDER BY
+	encounter_count DESC
+;
